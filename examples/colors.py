e571a5a1ccb0ebc0ed296cb0ee71523fb7f74de6
Gabriel <gabriel@gomide.org>
2022-05-28 11:43:53 -0300

    Added explanatory comments to examples/colors.py

--- examples/colors.py
+++ examples/colors.py
@@ -3,18 +3,23 @@ import sys
 
 sys.path.append('../Spike')
 
-import spike
+import spike # Import the GUI library
 
+# Create a 300x300px screen with the title 'Colors'
 surface = pygame.display.set_mode((300, 300))
 pygame.display.set_caption('Colors')
 
 run = True
 
+# Main loop
 while run:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-	surface.fill(spike.RED) # Sets the background color to red using spike's colors
-	pygame.display.update()
+    for event in pygame.event.get():
+	if event.type == pygame.QUIT:
+            # Exit the main loop when the player presses the 'X'
+            # at the top of the screen
+	    run = False
+
+    surface.fill(spike.RED) # Sets the background color to red using spike's colors
+    pygame.display.update() # Updated the screen
 	
 
